3663a84b290de02c0d340694fdd520ceaab6044b
João Augusto Colasso Handocha <joaoaugustocolassohandocha@gmail.com>
2025-05-19 12:01:37 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,6 @@
 import streamlit as st
 import os
 
-# Page title
-st.title('Product Registration')
-
-# Sidebar for selecting actions
-option = st.sidebar.selectbox('Select an option', ('Add Product', 'List Products'))
-
-# Simulated dictionary of a products database
-products = [
-
-    {'ID': 1, 'Name': 'Pineapple', 'Price': 5.99},
-    {'ID': 2, 'Name': 'Avocado', 'Price': 4.99},
-    {'ID': 3, 'Name': 'Banana', 'Price': 1.99}
-  
-]
-
 # Function to list products
 def list_products():
     
@@ -41,6 +26,22 @@ def add_product():
         products.append(new_product)
         
         st.sucess('Product added successfully!')
+
+# Page title
+st.title('Product Registration')
+
+# Sidebar for selecting actions
+option = st.sidebar.selectbox('Select an option', ('Add Product', 'List Products'))
+
+# Simulated dictionary of a products database
+products = [
+
+    {'ID': 1, 'Name': 'Pineapple', 'Price': 5.99},
+    {'ID': 2, 'Name': 'Avocado', 'Price': 4.99},
+    {'ID': 3, 'Name': 'Banana', 'Price': 1.99}
+  
+]
+
 
 # Logic for selected options in the sidebar
 if option == 'Add product':
